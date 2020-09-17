536fa56fe4439d567855a53242bcf1d26ef6e24b
Jesus Alejandro <jesus.garcia1953@alumnos.udg.com.mx>
2020-09-17 09:44:17 -0500

    Implementacion opcion 1

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+void capturarEnteros(){
+    int enteros[5];
+    float promedio;
+    int suma = 0;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("\tEntero [%li] : ", i+1);
+        scanf("%i", &enteros[i]);
+    }
+    printf("\t+-------------------------------------+\n");
+    printf("\t|         IMPRESION DE NUMEROS        |\n");
+    printf("\t+-------------------------------------+\n\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("\t%i\n", enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma = suma + enteros[i]; // suma += enteros[i]
+    }
+    printf("\tSuma: %i\n", suma);
+    printf("\tPromedio: %f\n\n", suma/5.0);
+    getchar();
+}
+
 int main (){
     char op;
 
@@ -19,7 +45,10 @@ int main (){
         switch (op)
         {
         case '1':
-           
+            printf("\t+-------------------------------------+\n");
+            printf("\t|         CAPTURA ENTEROS             |\n");
+            printf("\t+-------------------------------------+\n\n");
+            capturarEnteros();
             break;
         case '2':
             
